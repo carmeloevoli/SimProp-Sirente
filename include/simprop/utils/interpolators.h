a94410f08dc896f7674ab3af8ef03c104f491bec
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-11-13 09:51:00 +0100

    adding cspline interpolator

--- include/simprop/utils/interpolators.h
+++ include/simprop/utils/interpolators.h
@@ -8,6 +8,8 @@ namespace utils {
 
 double interpolate(double x, const std::vector<double> &X, const std::vector<double> &Y);
 
+double cspline(double x, const std::vector<double> &X, const std::vector<double> &Y);
+
 double interpolateEquidistant(double x, double lo, double hi, const std::vector<double> &Y);
 
 double interpolate2d(double x, double y, const std::vector<double> &X, const std::vector<double> &Y,
