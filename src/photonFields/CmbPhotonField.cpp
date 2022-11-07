1576f21a3087218cab68c3ea25a60e1d6ecc35df
Carmelo Evoli <carmelo.evoli@gssi.it>
2022-11-07 11:57:12 +0100

    minor fixing

--- src/photonFields/CmbPhotonField.cpp
+++ src/photonFields/CmbPhotonField.cpp
@@ -12,16 +12,16 @@ CMB::CMB(double T)
 }
 
 double CMB::density(double ePhoton, double z) const {
-  constexpr double factor = 1. / pow2(M_PI) / pow3(SI::hbarC);
   if (ePhoton < m_epsRange.first || ePhoton > m_epsRange.second) return 0;
+  constexpr double factor = 1. / pow2(M_PI) / pow3(SI::hbarC);
   const auto kT = SI::kBoltzmann * m_temperature;
   const auto density = factor * pow2(ePhoton) / std::expm1(ePhoton / kT);
   return std::max(density, 0.);
 }
 
 double CMB::I_gamma(double ePhoton, double z) const {
-  constexpr double factor = 1. / pow2(M_PI) / pow3(SI::hbarC);
   if (ePhoton < m_epsRange.first || ePhoton > m_epsRange.second) return 0;
+  constexpr double factor = 1. / pow2(M_PI) / pow3(SI::hbarC);
   const auto kT = SI::kBoltzmann * m_temperature;
   const auto I = kT * factor * (-std::log(1. - std::exp(-ePhoton / kT)));
   return std::fabs(I);
