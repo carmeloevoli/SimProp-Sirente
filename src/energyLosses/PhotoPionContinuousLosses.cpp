60d2906da7ae2c5d21dffb565e01bdaced16502d
Carmelo Evoli <carmelo.evoli@gssi.it>
2023-01-12 16:26:17 +0100

    using fitting function for Y(s)

--- src/energyLosses/PhotoPionContinuousLosses.cpp
+++ src/energyLosses/PhotoPionContinuousLosses.cpp
@@ -11,17 +11,17 @@ namespace simprop {
 namespace losses {
 
 double averageAngle(double s) {
-  if (s < 1.16 * SI::GeV2) return 0.;
-  if (s > 1e3 * SI::GeV2) return -1;
+  if (s > 1e4 * SI::GeV2) return -1;
   const double b = 12. / SI::GeV2;
   double I_mu = utils::QAGIntegration<double>(
-      [b, s](double mu) { return mu * std::exp(b * mu2t(mu, s)); }, -1., 1., 1000, 1e-8);
+      [b, s](double mu) { return mu * std::exp(b * mu2t(mu, s)); }, -1., 1., 1000, 1e-5);
   double I = utils::QAGIntegration<double>([b, s](double mu) { return std::exp(b * mu2t(mu, s)); },
-                                           -1., 1., 1000, 1e-8);
+                                           -1., 1., 1000, 1e-5);
   return I_mu / I;
 }
 
 double inelasticity(double s) {
+  if (s < 1.1519 * SI::GeV2) return 0.;
   auto sqrts = std::sqrt(s);
   auto E_pi = (s - pow2(SI::protonMassC2) + pow2(SI::pionMassC2)) / 2. / sqrts;
   auto p_pi = std::sqrt((s - pow2(SI::protonMassC2 + SI::pionMassC2)) *
@@ -31,6 +31,16 @@ double inelasticity(double s) {
   auto cosTheta_pi = averageAngle(s);
   return (s - pow2(SI::protonMassC2) + pow2(SI::pionMassC2)) / 2. / s *
          (1. + beta_pi * cosTheta_pi);
+}
+
+double inelasticityFitted(double s) {
+  if (s < 1.1519 * SI::GeV2) return 0.;
+  // using https://en.wikipedia.org/wiki/Fano_resonance
+  const double ds = s / SI::GeV2 - 1.15;
+  const double q = 1.03;
+  const double R = 0.80;
+  auto value = pow2(0.5 * q * R + ds) / (pow2(0.5 * R) + pow2(ds));
+  return 0.1014 * value / (s / SI::GeV2);
 }
 
 double inelasticityPoorApproximation(double s, double cosTheta_pi) {
@@ -68,7 +78,7 @@ double PhotoPionContinuousLosses::computeBetaComoving(PID pid, double Gamma, dou
           [&](double lnEpsPrime) {
             auto epsPrime = std::exp(lnEpsPrime);
             auto s = pow2(SI::protonMassC2) + 2. * SI::protonMassC2 * epsPrime;
-            return epsPrime * epsPrime * inelasticity(s) * m_xs.getAtS(pid, s) *
+            return epsPrime * epsPrime * inelasticityFitted(s) * m_xs.getAtS(pid, s) *
                    phField->I_gamma(epsPrime / 2. / Gamma, z);
           },
           lnEpsPrimeMin, lnEpsPrimeMax, 500);
