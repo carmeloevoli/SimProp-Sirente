4d6e0b4962b32db72a7763529c83af09536cf453
carmeloevoli <carmelo.evoli@gssi.it>
2023-02-03 14:50:57 +0100

    minor changes

--- apps/printAnalyticalSolution.cpp
+++ apps/printAnalyticalSolution.cpp
@@ -7,6 +7,7 @@ void testCharacteristics() {
   {
     utils::OutputFile out("proton_characteristics_redshift.txt");
     auto z = utils::LogAxis(1e-4, 5., 1000);
+    out << "# z - 10^17 eV - 10^18 eV - 10^19 eV - 10^20 eV - 10^21 eV\n";
     for (const auto &z_i : z) {
       out << std::scientific << z_i << "\t";
       out << b.generationEnergy(1e17 * SI::eV, z_i, 1e-6) / SI::eV << "\t";
@@ -20,6 +21,7 @@ void testCharacteristics() {
   {
     utils::OutputFile out("proton_characteristics_energy.txt");
     auto E = utils::LogAxis(1e16 * SI::eV, 1e22 * SI::eV, 1000);
+    out << "# E - 0.05 - 0.5 - 1 - 2 - 3\n";
     for (const auto &E_i : E) {
       out << std::scientific << E_i / SI::eV << "\t";
       out << b.generationEnergy(E_i, 0.05, 1e-6) / SI::eV << "\t";
@@ -36,6 +38,7 @@ void testJacobian() {
   solutions::Beniamino b(true);
   utils::OutputFile out("proton_dilation_factor.txt");
   auto z = utils::LogAxis(1e-3, 1e1, 100);
+  out << "# z\n";
   for (auto it = z.rbegin(); it != z.rend(); it++) {
     auto z_i = *it;
     out << std::scientific << z_i << "\t";
@@ -57,10 +60,10 @@ void testSpectrum() {
     for (const auto &E_i : E) {
       std::cout << E_i / SI::eV << "\n";
       out << std::scientific << E_i / SI::eV << "\t";
-      out << b.computeFlux(E_i, 0.05) << "\t";
-      out << b.computeFlux(E_i, 0.5) << "\t";
-      out << b.computeFlux(E_i, 1.0) << "\t";
-      out << b.computeFlux(E_i, 3.0) << "\t";
+      out << b.computeFlux(E_i, 0, 0.05) << "\t";
+      out << b.computeFlux(E_i, 0, 0.5) << "\t";
+      out << b.computeFlux(E_i, 0, 1.0) << "\t";
+      out << b.computeFlux(E_i, 0, 3.0) << "\t";
       out << "\n";
     }
   }
@@ -83,7 +86,7 @@ int main() {
     utils::startup_information();
     utils::Timer timer("main timer for analytical solution");
     testCharacteristics();
-    // testJacobian();
+    testJacobian();
     // testSpectrum();
   } catch (const std::exception &e) {
     LOGE << "exception caught with message: " << e.what();
