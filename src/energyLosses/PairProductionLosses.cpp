fe5c0cc6d19b5f02d82a41409916bb988fdbd7a2
Carmelo Evoli <carmelo.evoli@gssi.it>
2023-10-07 14:01:09 +0200

    improve energy losses precision

--- src/energyLosses/PairProductionLosses.cpp
+++ src/energyLosses/PairProductionLosses.cpp
@@ -79,7 +79,7 @@ double PairProductionLosses::computeProtonBeta(double Gamma,
           auto k = std::exp(lnk);
           return phi(k) / k * phField->density(k / TwoGamma_mec2, z);
         },
-        lkmin, lkmax, 8, 1e-2);
+        lkmin, lkmax, 9, 1e-3);
   }
   constexpr auto factor = SI::alpha * pow2(SI::electronRadius) * SI::cLight * SI::electronMassC2 *
                           (SI::electronMass / SI::protonMass);
